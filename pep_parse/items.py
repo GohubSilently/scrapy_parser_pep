82dc66b59bb8d5a4ffc2871b1ee27486550451fa
Vadim <khalinvadim3@gmail.com>
2025-12-24 10:40:55 +0300

    Add class PepParseItem in items.py;

--- pep_parse/items.py
+++ pep_parse/items.py
@@ -1,7 +1,7 @@
-import scrapy
+from scrapy.item import Item, Field
 
 
-class PepParseItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class PepParseItem(Item):
+    number = Field()
+    name = Field()
+    status = Field()
