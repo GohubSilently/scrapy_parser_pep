bfc418b4a0b68fe6a77a20c25f49374962ac70db
Vadim <khalinvadim3@gmail.com>
2025-12-26 21:03:52 +0300

    Code Review #2;

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -4,13 +4,13 @@ from pathlib import Path
 BOT_NAME = 'pep_parse'
 
 NEWSPIDER_MODULE = 'pep_parse.spiders'
-SPIDER_MODULES = [f'{NEWSPIDER_MODULE}']
+SPIDER_MODULES = [NEWSPIDER_MODULE]
 
 FEED_EXPORT_ENCODING = 'utf-8'
 
 BASE_DIR = Path(__file__).parent.parent
 RESULTS = 'results'
-RESULTS_DIR = BASE_DIR / 'results'
+RESULTS_DIR = BASE_DIR / RESULTS
 
 FEEDS = {
     f'{RESULTS}/pep_%(time)s.csv': {
